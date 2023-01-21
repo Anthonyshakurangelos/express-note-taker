4d88a2b821e05c664596770d9bbe60f5bf8d0ba7
Anthonyshakurangelos <anthonyshakurangelos@gmail.com>
2023-01-21 10:03:31 -0700

    deleted an extra . for folder route just need to post to work and heroku

--- routes/notes.js
+++ routes/notes.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 
 // this is where routes go
 router.get("/", (req, res) => {
-  fs.readFile('../db/db.json', 'utf-8', (err, data) => {
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
     if (err){console.log(err)}
     else {res.json(JSON.parse(data))}
   })
@@ -14,23 +14,23 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
     const {title, text} = req.body;
-    const db = JSON.parse(fs.readFileSync('../db/db.json', (err) => { if (err) console.log(err)}));
+    const db = JSON.parse(fs.readFileSync('./db/db.json', (err) => { if (err) console.log(err)}));
     const item = {
         id: uuidv4(), 
         title,
         text
     }
     db.push(item);
-    fs.writeFileSync('../db/db.json', JSON.stringify(db, null, 2));
+    fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 2));
     res.json(item);
 });
 
 router.delete('/:id', (req, res) => {
-        const db = JSON.parse(fs.readFileSync('../db/db.json', (err) => { if (err) console.log(err)}));
+        const db = JSON.parse(fs.readFileSync('./db/db.json', (err) => { if (err) console.log(err)}));
          const newDb = db.filter((note) => {
             return note.id !== req.params.id;
         });
-        fs.writeFileSync('../db/db.json', JSON.stringify(newDb, null, 2));
+        fs.writeFileSync('./db/db.json', JSON.stringify(newDb, null, 2));
         res.json({message: `Deleted ${req.params.id}`});
     });
 
